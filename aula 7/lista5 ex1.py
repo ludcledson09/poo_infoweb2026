8c3ce50378d60b7603c3fe992107a176154b3f4e
ludcledson09 <jose.ludcledson@escolar.ifrn.edu.br>
2026-05-26 18:51:32 +0000

    autalização lista 5 aula 7

--- aula 7/lista5 ex1.py
+++ aula 7/lista5 ex1.py
@@ -28,44 +28,77 @@ class Paciente:
     def get_nascimento(self): return self.__nascimento
     def __str__(self):
         return f"{self.__id} - {self.__nome} - {self.__cpf} - {self.__telefone} - \
-{self._nascimento.strftime("%d/%m%Y")}"
+{self.__nascimento.strftime("%d/%m/%Y")}"
     def idade(self):
-       tempo = datetime.now() - self._nascimento #timedelta é contado em dias
+       tempo = datetime.now() - self.__nascimento #timedelta é contado em dias
        anos = tempo.days // 365
-       meses = tempo.days %365 // 0
+       meses = tempo.days % 365 // 30
        return f"Idade: {anos} anos(s) e {meses} mes(es)"
-    
-x = Paciente(1,"Eduardo","001.002.003-45","84-90009", datetime(2010,1,25))
-print(x)
-print(x.idade())
-
 class PacienteUI:
    __pacientes = []
    @staticmethod
    def main():
       op = 0
       while op != 9:
-           op = PacienteUI()
+           op = PacienteUI.menu()
            if op==1:PacienteUI.inserir()
-           if op==2:PacienteUI.instalar()
+           if op==2:PacienteUI.listar()
+           if op==3:PacienteUI.atualizar()
+           if op==4:PacienteUI.excluir()
+           if op==5:PacienteUI.pesquisar()
+           if op==6:PacienteUI.aniversariantes()
       
    @staticmethod
    def menu():
       print("1-inserir,2-listar,3-atualizar,4-excluir,5-pesquisar,6-aniversariante,9-fim")
       return int(input("informe uma opção: "))
    
-   @staticmethod
+   @classmethod
    def inserir(cls):
        id =int(input("informe o id: "))
        nome =(input("informe o nome: "))
        cpf =int(input("informe o CPF: "))
        fone =(input("informe o telefone: "))
-       nasc = datetime.strptime(input("Informe a data de nascimento"),"%d/%m/%Y")
+       nasc = datetime.strptime(input("Informe a data de nascimento: "),"%d/%m/%Y")
        x =Paciente(id,nome,cpf,fone,nasc)
        cls.__pacientes.append(x)
     
    @classmethod
-   def list(cls):
+   def listar(cls):
       for x in cls.__pacientes:print(x, x.idade())
 
+   @classmethod
+   def atualizar(cls):
+       for x in cls.__pacientes: print(x)
+       id =int(input("informe o id do paciente a ser atualizado: "))
+       for x in cls.__pacientes:
+         if x.get_id() ==id:
+          nome =(input("informe o novo nome: "))
+          cpf =int(input("informe o novo CPF: "))
+          fone =(input("informe o novo telefone: "))
+          nasc = datetime.strptime(input("Informe a nova data de nascimento: "),"%d/%m/%Y")
+          x.set_nome(nome)
+          x.set_cpf(cpf)
+          x.set_telefone(fone)
+          x.set_nascimento(nasc)
+   @classmethod
+   def excluir(cls):
+      for x in cls.__pacientes: print(x)
+      id =int(input("informe o id do paciente a ser atualizado: "))
+      for x in cls.__pacientes:
+        if x.get_id() ==id:
+          cls.__pacientes.remove(x)
+    
+   @classmethod
+   def pesquisar(cls):
+     s=input("informe as iniciais do nome: ")
+     for x in cls.__pacientes:
+        if x.get_nome().startswit(s):print(x)
+
+   @classmethod
+   def aniversariantes(cls):
+      m = int(input("input o mês para a listar de aniversaiantes (1-12): "))
+      for x in cls.__pacientes:
+          if x.get_nascimento().month == m: print(x)
+     
 PacienteUI.main()
