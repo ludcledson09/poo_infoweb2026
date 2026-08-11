8654fc500d990208c3bf25e91b1493abbdf164ce
ludcledson09 <jose.ludcledson@escolar.ifrn.edu.br>
2026-08-11 18:44:00 +0000

    Aula 10

---
+++ Aula_10/index.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from retanguloUI import RetanguloUI
+
+RetanguloUI.main()
+
+#st.header("POO em Python com Streamlit")
+
+#if st.button("Clique aqui"):
+#    st.write("Bem-vindo(a) ao Streamlit")
+    
